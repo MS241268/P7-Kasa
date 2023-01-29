bedf2221a108cc6f7500b30a8a03561f581899bf
Schonne Marc <schonne.marc@orange.fr>
2023-01-29 15:10:45 +0100

    Page 'Home': Ajout animation sur titre 'h1' de la bannière

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,13 +9,12 @@ function Home() {
 	<main>
 		<section className = "homeBanner">
 			<img src = { homeBanner } alt = "Forêt et rochers en bord de mer"/>
-			<h1>Chez vous, partout et ailleurs</h1>
+			<h1 className = "titleBannerEffect">Chez vous, partout et ailleurs</h1>
 			<div className = "homeBannerBackGround"></div>
 		</section>
 
 		<section className="cards_section">
 			{ housingsDataBase.map ((housingData, index) => {
-				console.log(housingsDataBase, 'index :'+index)
 				return (
 					<NavLink key={ index } to = { `/products/${housingData.id}` }>
 					<Cards key = { index } cover = { housingData.cover } title = { housingData.title }>
