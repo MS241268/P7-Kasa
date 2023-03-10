de177c5e4e08a75907ff67dd2fb4979578c61cdf
Schonne Marc <schonne.marc@orange.fr>
2023-03-11 00:38:00 +0100

    Synchronisation fichier

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
           <Route path="products/:productId" element={<Housing />}/>
-          <Route path="*" element={<Error />} />
+          <Route path="*" element={<Error />}/>
       </Routes>
       
       <Footer />
