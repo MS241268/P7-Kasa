a545ce5e2e65b34189cd71f1f5b59a9a3abfebef
Schonne Marc <schonne.marc@orange.fr>
2023-05-19 12:07:43 +0200

    Composant Slider: Ajout d'un diaporama pour les photos de présentation l'appartement sélectionné

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useCallback, useState, useEffect} from 'react'
 import rightArrow from '../../assets/right_arrow.svg'
 import leftArrow from '../../assets/left_arrow.svg'
 import './index.css'
@@ -9,8 +9,21 @@ function Slider({slides}) {
 	const previousPicture=() => {
 		setCurrentPicture (currentPicture !== 0 ? currentPicture - 1 : (slides.length-1))}/*Condition si clic sur la flèche 'previous' et 1ère image courante => Retour sur la dernière image*/
 
-	const nextPicture=() => {
-		setCurrentPicture (currentPicture !== (slides.length - 1) ? currentPicture + 1 : 0)}/*Condition si clic sur la flèche 'next' et dernière image courante => Retour sur la 1ère image*/
+	const nextPicture = useCallback(() => {
+		setCurrentPicture (currentPicture !== (slides.length - 1) ? currentPicture + 1 : 0)},/*Condition si clic sur la flèche 'next' et dernière image courante => Retour sur la 1ère image*/
+		[currentPicture,slides.length])//Déclaration des dépendances : servent à observer les arguments pour lesquels la fonction doit s'exécuter
+	
+	//Timer slider automatique
+	useEffect(() => {
+		if (slides.length > 1){
+			const interval = setInterval(() => {
+				nextPicture(currentPicture,slides.length)
+			},5000)
+			return () => {
+				clearInterval(interval)
+			}
+		}
+	},[currentPicture, nextPicture, slides.length])//Facultatif car les dépendences "[...]" sont déjà dans la fonction "nextPicture"
 
 	return (
 		<section className="slider">
